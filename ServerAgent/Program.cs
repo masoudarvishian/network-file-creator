a992742c645ac6b6e310713f9227ed6e76768de1
Masoud Darvishian <masoudarvishian@gmail.com>
2021-08-15 07:03:42 +0430

    initial create

---
+++ ServerAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
